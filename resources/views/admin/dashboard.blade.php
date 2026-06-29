216a08f4eeae0fe3189e6cc0e60a28632b791802
Daniel <dbrito4321@gmail.com>
2026-06-29 18:20:11 -0300

    fix: ajusta nome e cor de fundo do navbar e cor de fundo do card

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,56 +1,56 @@
 @extends('admin.layout')
 
-@section('titulo', 'Dashboard')    
-    
+@section('titulo', 'Dashboard')
+
 @section('conteudo')
-    
+
 
     <div class="row container">
         <section class="info">
 
-        <div class="col s12 m4">
-        <article class="bg-gradient-green card z-depth-4 ">
-            <i class="material-icons">paid</i>
-            <p>Faturamento</p>
-            <h3>R$ 123,00</h3>       
-        </article>
-        </div>
-
-        <div class="col s12 m4">
-            <article class="bg-gradient-blue card z-depth-4 ">
-            <i class="material-icons">face</i>
-            <p>Usuários</p>
-            <h3>{{ $users }}</h3>           
-            </article>
+            <div class="col s12 m4">
+                <article class="bg-gradient-blue card z-depth-4 ">
+                    <i class="material-icons">paid</i>
+                    <p>Faturamento</p>
+                    <h3>R$ 123,00</h3>
+                </article>
             </div>
 
             <div class="col s12 m4">
-            <article class="bg-gradient-orange card z-depth-4 ">
-                <i class="material-icons">shopping_cart</i>
-                <p>Pedidos este mês</p>
-                <h3>0</h3>            
-            </article>
+                <article class="bg-gradient-orange card z-depth-4 ">
+                    <i class="material-icons">face</i>
+                    <p>Usuários</p>
+                    <h3>{{ $users }}</h3>
+                </article>
             </div>
 
-        </section>        
+            <div class="col s12 m4">
+                <article class="bg-gradient-red card z-depth-4 ">
+                    <i class="material-icons">shopping_cart</i>
+                    <p>Pedidos este mês</p>
+                    <h3>0</h3>
+                </article>
+            </div>
+
+        </section>
     </div>
 
 
-        <div class="row container ">
-            <section class="graficos col s12 m6" >            
-                <div class="grafico card z-depth-4">
-                    <h5 class="center"> Aquisição de usuários</h5>
-                    <canvas id="myChart" width="400" height="200"></canvas>
-                </div>           
-            </section> 
-            
-            <section class="graficos col s12 m6">            
-                <div class="grafico card z-depth-4">
-                    <h5 class="center"> Produtos </h5>
-                <canvas id="myChart2" width="400" height="200"></canvas> 
-            </div>            
-            </section>             
-        </div>
+    <div class="row container ">
+        <section class="graficos col s12 m6">
+            <div class="grafico card z-depth-4">
+                <h5 class="center"> Aquisição de usuários</h5>
+                <canvas id="myChart" width="400" height="200"></canvas>
+            </div>
+        </section>
+
+        <section class="graficos col s12 m6">
+            <div class="grafico card z-depth-4">
+                <h5 class="center"> Produtos </h5>
+                <canvas id="myChart2" width="400" height="200"></canvas>
+            </div>
+        </section>
+    </div>
 
 
     </div>
@@ -59,7 +59,6 @@
 
 @push('graficos')
     <script>
-        
         /* Gráfico 01 */
         var ctx = document.getElementById('myChart');
         var myChart = new Chart(ctx, {
@@ -68,20 +67,20 @@
                 labels: [{{ $userAno }}],
                 datasets: [{
                     label: [{!! $userLabel !!}],
-                    data: [{{ $userTotal}}],
+                    data: [{{ $userTotal }}],
                     backgroundColor: [
                         'rgba(255, 99, 132, 1)',
                         'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',                         
+                        'rgba(255, 206, 86, 1)',
                         'rgba(255, 159, 64, 1)'
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
                         'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',                     
+                        'rgba(255, 206, 86, 1)',
                         'rgba(255, 159, 64, 1)'
                     ],
-                borderWidth: 1, 
+                    borderWidth: 1,
                 }]
             },
             options: {
@@ -106,12 +105,11 @@
                     data: @json($catTotal),
                     backgroundColor: [
                         'rgba(255, 99, 132)',
-                        'rgba(54, 162, 235)',                         
+                        'rgba(54, 162, 235)',
                         'rgba(255, 159, 64)'
                     ]
                 }]
             }
-        });  
-
-    </script>  
+        });
+    </script>
 @endpush
